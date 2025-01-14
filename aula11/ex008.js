9ce65a1e5e42a66150a3ce3c98a1ac1d7c68eb92
Carolina Zampieri Rodrigues <carolina.zampieri89@gmail.com>
2025-01-14 16:39:43 -0300

    Add files via upload

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60.5
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel > 60) { //condição simples
+    console.log(`Você ultrapassou a velocidade permitida. Você foi multado.`) 
+}
+console.log(`Dirija com segurança!`)
---
+++ aula13/ambiente.js
@@ -0,0 +1,11 @@
+var contador = 1
+while (contador < 10) {
+    console.log(`Passo ${contador}`)
+    contador++
+}
+
+var contador2 = 1
+do {
+    console.log(`Passo ${contador2}`)
+    contador2++
+} while (contador2 <= 6)
