28129a06578c6f6d9863466fde8aed4ecc63d5bd
Carolina Zampieri Rodrigues <carolina.zampieri89@gmail.com>
2025-01-14 18:30:36 -0300

    Add files via upload

---
+++ curso-js/aula15/ambiente.js
@@ -0,0 +1,9 @@
+let num = [5, 8, 2, 9, 3]
+num.push(7)
+num.sort()
+
+//acrescentando valor
+num[5] = 6 //aparece por último, pois segue a ordem 
+
+console.log(num)
+console.log(num[3])
---
+++ curso-js/aula16/funcao03.js
@@ -0,0 +1,7 @@
+//Passar um parametro para a variavel
+//e conseguir executar uma função
+let v = function(x) {
+    return x * 2
+}
+
+console.log(v(5))
